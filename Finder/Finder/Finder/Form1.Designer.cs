8936bb5bce69426c2960d98d2584ed78871dd318
Fistashk0 <pasetikus@gmail.com>
2015-02-11 13:19:25 +0400

    first commit

---
+++ Finder/Finder/Finder/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Finder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (folderTxtbox.Text != "")
+            {
+                Finder.Properties.Settings.Default.Setting = folderTxtbox.Text;
+            }
+
+            if (filterTxtbox.Text != "")
+            {
+                Finder.Properties.Settings.Default.Setting1 = filterTxtbox.Text;
+            }
+
+            if (textTxtbox.Text != "")
+            {
+                Finder.Properties.Settings.Default.Setting2 = textTxtbox.Text;
+            }
+            if (treeView1 != null)
+            {
+                Finder.Properties.Settings.Default.Setting3 = treeView1;
+            }
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            Finder.Properties.Settings.Default.Save();
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textTxtbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.filterTxtbox = new System.Windows.Forms.TextBox();
+            this.folderTxtbox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView1.Location = new System.Drawing.Point(1, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(123, 197);
+            this.treeView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(224, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 21);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textTxtbox
+            // 
+            this.textTxtbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textTxtbox.Location = new System.Drawing.Point(133, 114);
+            this.textTxtbox.Name = "textTxtbox";
+            this.textTxtbox.Size = new System.Drawing.Size(175, 20);
+            this.textTxtbox.TabIndex = 3;
+            this.textTxtbox.Text = "Введите текст в файле";
+            this.textTxtbox.Click += new System.EventHandler(this.textTxtbox_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(130, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 21);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Директория";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // filterTxtbox
+            // 
+            this.filterTxtbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filterTxtbox.Location = new System.Drawing.Point(133, 83);
+            this.filterTxtbox.Name = "filterTxtbox";
+            this.filterTxtbox.Size = new System.Drawing.Size(175, 20);
+            this.filterTxtbox.TabIndex = 2;
+            this.filterTxtbox.Text = "Введите имя файла";
+            this.filterTxtbox.Click += new System.EventHandler(this.filterTxtbox_Click);
+            // 
+            // folderTxtbox
+            // 
+            this.folderTxtbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.folderTxtbox.Location = new System.Drawing.Point(133, 57);
+            this.folderTxtbox.Name = "folderTxtbox";
+            this.folderTxtbox.Size = new System.Drawing.Size(175, 20);
+            this.folderTxtbox.TabIndex = 1;
+            this.folderTxtbox.Text = "Введите путь";
+            this.folderTxtbox.Click += new System.EventHandler(this.folderTxtbox_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 221);
+            this.Controls.Add(this.folderTxtbox);
+            this.Controls.Add(this.filterTxtbox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textTxtbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.MinimumSize = new System.Drawing.Size(336, 259);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textTxtbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox filterTxtbox;
+        private System.Windows.Forms.TextBox folderTxtbox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
+
---
+++ Finder/Finder/Finder/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Finder
+{
+    public partial class Form1 : Form
+    {
+     
+        private Stopwatch sw = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string message="";
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog op = new FolderBrowserDialog();
+            if (op.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                folderTxtbox.Text = op.SelectedPath.ToString();
+                string[] textforstartnode = folderTxtbox.Text.Split('\\');
+                treeView1.Nodes.Add(textforstartnode[textforstartnode.Length-1]);
+            }  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (!backgroundWorker1.IsBusy)
+                {
+                    backgroundWorker1.RunWorkerAsync();
+                    button1.Text = "Стоп";
+                    sw.Start();
+                    timer1.Start();
+                }
+                else { 
+
+                    backgroundWorker1.CancelAsync();
+                    button1.Text = "Остановка";
+                    button1.Enabled = false;
+                }
+                
+            }
+            catch { }
+        }
+        
+        private string[] findall() 
+        {
+            string[] allFoundFiles = Directory.GetFiles(folderTxtbox.Text.ToString(), filterTxtbox.Text.ToString() + "*", SearchOption.AllDirectories);
+            return allFoundFiles;
+        }
+        private void addfolder(string text,TreeNode tn) 
+        {
+            tn.Nodes.Add(text);
+        }
+        private bool findintext(string path) 
+        {
+            StreamReader str = new StreamReader(path, Encoding.Default);
+            while (!str.EndOfStream)
+            {
+                string st = str.ReadLine();
+                if (st.Contains(textTxtbox.Text.ToString()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private Int32 isit(string s,TreeNode tn) 
+        {
+            Int32 i = 0;
+            while (i < tn.Nodes.Count)
+            {
+                if (s == tn.Nodes[i].Text)
+                { 
+                    return i; }
+                i++;
+            }
+            return -1;
+        }
+        private void addtotreenode(string[] text,TreeNode tn,Int32 count) 
+        {
+            if (count < text.Length)
+            {
+                if (tn != null)
+                {
+                    if (tn.Nodes.Count == 0)
+                    {
+                        addfolder(text[count], tn);
+                        addtotreenode(text, tn.Nodes[tn.Nodes.Count - 1], count + 1);
+                    }
+                    else
+                    {
+                        Int32 thcount = isit(text[count], tn);
+                        if (thcount != -1)
+                        {
+                            addtotreenode(text, tn.Nodes[thcount], count + 1);
+                        }
+                        else
+                        {
+                            addfolder(text[count], tn);
+                            addtotreenode(text, tn.Nodes[tn.Nodes.Count - 1], count + 1);
+                        }
+                    }
+
+                }
+            }
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            message = "Выполняется поиск всех файлов с заданным именем";
+            string[] pathches = findall();
+            Int32 i = 0;
+            Int32 all=0;
+            string current = "";
+            while (backgroundWorker1.CancellationPending == false && i != pathches.Length) 
+            {
+                string s = pathches[i].Substring(folderTxtbox.Text.ToString().Length);
+                string[] ss = s.Split('\\');
+                current = ss[ss.Length - 1];
+                message = "В данный момент обрабатывается файл : " + current.ToString() + " . Всего найдено файлов " + all.ToString();
+                all++;
+                if (findintext(pathches[i])) 
+                {
+                    
+                   Invoke(new MethodInvoker(delegate
+                    {
+                        addtotreenode(ss, treeView1.TopNode, 1);
+                    }));
+                }
+                i++;
+            }
+            message = "Задача завершена. Всего найдено  " + all.ToString() + " файлов.";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Finder.Properties.Settings.Default.Setting != "") 
+            {
+                folderTxtbox.Text = Finder.Properties.Settings.Default.Setting;
+            }
+
+            if (Finder.Properties.Settings.Default.Setting1 != "")
+            {
+                filterTxtbox.Text = Finder.Properties.Settings.Default.Setting1;
+            }
+
+            if (Finder.Properties.Settings.Default.Setting2 != "")
+            {
+                textTxtbox.Text = Finder.Properties.Settings.Default.Setting2;
+            }
+            if (folderTxtbox.Text != "") { 
+
+            string[] textforstartnode = folderTxtbox.Text.Split('\\');
+            treeView1.Nodes.Add(textforstartnode[textforstartnode.Length - 1]);}
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (!backgroundWorker1.IsBusy)
+            {
+
+                button1.Enabled = true;
+                button1.Text = "Старт";
+                timer1.Stop();
+                sw.Stop();
+            }
+            label1.Text = message+" " +sw.Elapsed.Minutes.ToString()+ ":"+ sw.Elapsed.Seconds.ToString();
+        }
+
+        #region textboxcleaner
+        private void folderTxtbox_Click(object sender, EventArgs e)
+        {
+            if (folderTxtbox.Text == "Введите путь")
+            {
+                folderTxtbox.Text = "";
+            }
+        }
+
+        private void filterTxtbox_Click(object sender, EventArgs e)
+        {
+            if (filterTxtbox.Text == "Введите путь")
+            {
+                filterTxtbox.Text = "";
+            }
+        }
+
+        private void textTxtbox_Click(object sender, EventArgs e)
+        {
+            if (textTxtbox.Text == "Введите текст в файле")
+            {
+                textTxtbox.Text = "";
+            }
+
+        }
+        #endregion
+
+    }
+}
